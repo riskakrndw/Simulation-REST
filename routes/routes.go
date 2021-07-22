e32af19a42a1034265e2fff63400b48f9ce2bfe6
ivansusiyanto <ivansusiyanto@gmail.com>
2021-07-23 00:33:31 +0700

    Add DeleteUserController function

--- routes/routes.go
+++ routes/routes.go
@@ -11,6 +11,6 @@ func New(e *echo.Echo) {
 	e.GET("/users", controllers.GetUsersController)
 	// e.GET("/users/:id", controllers.GetUserController)
 	// e.POST("/users", controllers.CreateUserController)
-	// e.DELETE("/users/:id", controllers.DeleteUserController)
+	e.DELETE("/users/:id", controllers.DeleteUserController)
 	e.PUT("/users/:id", controllers.UpdateUserController)
 }
