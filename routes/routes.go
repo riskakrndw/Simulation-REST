1a6d5bc01b4744548cfa4069f5d86853fa72681a
Riska Kurnia Dewi <riskakurniad@gmail.com>
2021-07-22 20:34:58 +0700

    add template

---
+++ routes/routes.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"project/controllers"
+
+	"github.com/labstack/echo"
+)
+
+func New(e *echo.Echo) {
+	//routing
+	// e.GET("/users", controllers.GetUsersControllr)
+	// e.GET("/users/:id", controllers.GetUserController)
+	// e.POST("/users", controllers.CreateUserController)
+	// e.DELETE("/users/:id", controllers.DeleteUserController)
+	// e.PUT("/users/:id", controllers.UpdateUserController)
+}
