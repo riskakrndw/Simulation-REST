763cf65168d3c4e792bfeb3d5a9fdf0dc04ffec7
Riska Kurnia Dewi <riskakurniad@gmail.com>
2021-07-22 20:41:39 +0700

    add get all users

---
+++ controllers/GetUsersController.go
@@ -0,0 +1,19 @@
+package controllers
+
+import (
+	"net/http"
+	"project/lib/database"
+
+	"github.com/labstack/echo"
+)
+
+func GetUsersController(c echo.Context) error {
+	users, err := database.GetUsers()
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"status": "success",
+		"users":  users,
+	})
+}
--- lib/database/user.go
+++ lib/database/user.go
@@ -1,1 +1,14 @@
 package database
+
+import (
+	"project/config"
+	"project/models"
+)
+
+func GetUsers() (interface{}, error) {
+	var users []models.User
+	if err := config.DB.Find(&users).Error; err != nil {
+		return nil, err
+	}
+	return users, nil
+}
