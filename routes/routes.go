9478d195f6e241c3c74ff57147a984193569d6d5
inasalifatus <inasalifatus@gmail.com>
2021-07-22 22:58:39 +0700

    add createuser

--- routes/routes.go
+++ routes/routes.go
@@ -10,7 +10,7 @@ func New(e *echo.Echo) {
 	//routing
 	e.GET("/users", controllers.GetUsersController)
 	// e.GET("/users/:id", controllers.GetUserController)
-	// e.POST("/users", controllers.CreateUserController)
+	e.POST("/users", controllers.CreateUserController)
 	// e.DELETE("/users/:id", controllers.DeleteUserController)
 	e.PUT("/users/:id", controllers.UpdateUserController)
 }
